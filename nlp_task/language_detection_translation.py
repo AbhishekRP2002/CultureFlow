642f44913df3389963b4e03e5ae76f9dc3041ac7
Siddhant <siddhanthota22@gmail.com>
2023-05-06 19:32:21 +0530

    Added Language Detection & Translation

---
+++ nlp_task/language_detection_translation.py
@@ -0,0 +1,47 @@
+from google_trans_new import google_translator # pip install google_trans_new==1.1.9
+import streamlit as st # pip install streamlit==0.82.0
+import gtts # pip install gtts
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+Languages = {'afrikaans':'af','albanian':'sq','amharic':'am','arabic':'ar','armenian':'hy','azerbaijani':'az','basque':'eu','belarusian':'be','bengali':'bn','bosnian':'bs','bulgarian':'bg','catalan':'ca','cebuano':'ceb','chichewa':'ny','chinese (simplified)':'zh-cn','chinese (traditional)':'zh-tw','corsican':'co','croatian':'hr','czech':'cs','danish':'da','dutch':'nl','english':'en','esperanto':'eo','estonian':'et','filipino':'tl','finnish':'fi','french':'fr','frisian':'fy','galician':'gl','georgian':'ka','german':'de','greek':'el','gujarati':'gu','haitian creole':'ht','hausa':'ha','hawaiian':'haw','hebrew':'iw','hebrew':'he','hindi':'hi','hmong':'hmn','hungarian':'hu','icelandic':'is','igbo':'ig','indonesian':'id','irish':'ga','italian':'it','japanese':'ja','javanese':'jw','kannada':'kn','kazakh':'kk','khmer':'km','korean':'ko','kurdish (kurmanji)':'ku','kyrgyz':'ky','lao':'lo','latin':'la','latvian':'lv','lithuanian':'lt','luxembourgish':'lb','macedonian':'mk','malagasy':'mg','malay':'ms','malayalam':'ml','maltese':'mt','maori':'mi','marathi':'mr','mongolian':'mn','myanmar (burmese)':'my','nepali':'ne','norwegian':'no','odia':'or','pashto':'ps','persian':'fa','polish':'pl','portuguese':'pt','punjabi':'pa','romanian':'ro','russian':'ru','samoan':'sm','scots gaelic':'gd','serbian':'sr','sesotho':'st','shona':'sn','sindhi':'sd','sinhala':'si','slovak':'sk','slovenian':'sl','somali':'so','spanish':'es','sundanese':'su','swahili':'sw','swedish':'sv','tajik':'tg','tamil':'ta','telugu':'te','thai':'th','turkish':'tr','turkmen':'tk','ukrainian':'uk','urdu':'ur','uyghur':'ug','uzbek':'uz','vietnamese':'vi','welsh':'cy','xhosa':'xh','yiddish':'yi','yoruba':'yo','zulu':'zu'}
+
+completion = openai.ChatCompletion()
+
+def langDetect(text):
+    response = completion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a helpful AI assistant who will help the user to perform language translation and detection on their text data provided. Analyze the language and provide the result to the user. Detect the language of the text '{text}' and display it. Give the output in one word.",
+            },
+            {"role": "user", "content": text},
+        ],
+        temperature=0.5,
+    )
+
+    answer = response.choices[0].message["content"]
+
+    return answer
+
+def langTranslate(val1,val2,text):
+    response = completion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a helpful AI assistant who will help the user to perform language translation and detection on their text data provided. Analyze the language and provide the result to the user. Detect the language of {text} and display it. Then, Translate '{text}' from {val1} to {val2}. Provide only the translated text as output. ",
+            },
+            {"role": "user", "content": text},
+        ],
+        temperature=0.5,
+    )
+
+    answer = response.choices[0].message["content"]
+
+    return answer
