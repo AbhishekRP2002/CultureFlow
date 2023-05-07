0bb41fe381e2c6d38aec6233b6d4e0484c924e44
Siddhant <siddhanthota22@gmail.com>
2023-05-07 10:24:46 +0530

    made changes to info extraction

--- nlp_task/contact_info_extraction.py
+++ nlp_task/contact_info_extraction.py
@@ -27,32 +27,42 @@ def extract_from_excel(file):
                     numbers += extract_contact_numbers(value)
     return numbers
 
-def extract_from_csv(file):
-    df = pd.read_csv(file)
-    numbers = []
-    for col in df.columns:
-        for value in df[col]:
-            if isinstance(value, str):
-                numbers += extract_contact_numbers(value)
-    return numbers
+# def extract_from_csv(file):
+#     df = pd.read_csv(file)
+#     numbers = []
+#     for col in df.columns:
+#         for value in df[col]:
+#             if isinstance(value, str):
+#                 numbers += extract_contact_numbers(value)
+#     return numbers
 
-def extract_from_text(file):
-    with open(file.name, 'r') as f:
-        text = f.read()
-    return extract_contact_numbers(text)
+# def extract_from_text(file):
+#     with open(file.name, 'r') as f:
+#         text = f.read()
+#     return extract_contact_numbers(text)
 
-def extract_numbers(file):
-    if file.type == 'application/pdf':
-        numbers = extract_from_pdf(file)
-    elif file.type == 'text/plain':
-        numbers = extract_from_text(file)
-    elif file.type == 'text/csv':
-        numbers = extract_from_csv(file)
-    elif file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        numbers = extract_from_excel(file)
-    else:
-        numbers = []
-    return numbers
+def extract_contact_info(text):
+    name_pattern = re.compile(r'\b[A-Z][a-z]+\s[A-Z][a-z]+\b')
+    phone_pattern = re.compile(r'\b\d{10}\b|\b\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}\b|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}\b')
+    email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+
+    names = name_pattern.findall(text)
+    phone_numbers = phone_pattern.findall(text)
+    email_ids = email_pattern.findall(text)
+
+    return names, phone_numbers, email_ids
+
+def process_file(input_file):
+    data = pd.read_csv(input_file)
+    text = ' '.join(data.values.flatten())
+
+    names, phone_numbers, email_ids = extract_contact_info(text)
+
+    extracted_data = pd.DataFrame({'Name': names, 'Phone': phone_numbers, 'Email': email_ids})
+    extracted_data.to_csv('extracted_contacts.csv', index=False)
+
+    return extracted_data
+
 
 def display_numbers(numbers):
     if not numbers:
@@ -64,7 +74,32 @@ def display_numbers(numbers):
 
 def extractInfo(file):
         if file is not None:
-            numbers = extract_numbers(file)
-            display_numbers(numbers)
+            if file.type == 'application/pdf':
+                numbers = extract_from_pdf(file)
+                display_numbers(numbers)
+            
+            elif file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                numbers = extract_from_excel(file)
+                display_numbers(numbers)
+
+            elif file.type == 'text/plain':
+                text = file.read().decode('utf-8')
+                names, phone_numbers, email_ids = extract_contact_info(text)
+                st.write("Names:", names)
+                st.write("Phone Numbers:", phone_numbers)
+                st.write("Email IDs:", email_ids)
+            
+            elif file.type == "text/csv":
+                extracted_data = process_file(file)
+                st.write(extracted_data)
+
+                if st.button('Download Extracted Contacts'):
+                        st.download_button(
+                            label="Download CSV",
+                            data=extracted_data.to_csv(index=False).encode('utf-8'),
+                            file_name="extracted_contacts.csv",
+                            mime="text/csv",
+                        )
+            
         else:
             st.write("Give valid file!")
